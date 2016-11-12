eb75d324f73a88a639f5032890069ffbdaf0ed27
Maxime Stevenot <stevenot@ecole.ensicaen.fr>
2016-11-12 15:57:11 +0100

    Creation of Visual Studio solution

---
+++ AutoMix Software/AutoMix Data Extraction/AutoMix Data Extraction.h
@@ -0,0 +1,13 @@
+// AutoMix Data Extraction.h
+
+#pragma once
+
+using namespace System;
+
+namespace AutoMixDataExtraction {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
