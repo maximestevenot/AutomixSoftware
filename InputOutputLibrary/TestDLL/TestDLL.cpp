66520105c3a12d04ce019be3dba37a85a086bf32
Pierre Gabon <gabon@ecole.ensicaen.fr>
2017-01-02 18:13:23 +0100

    Input Output Module

---
+++ InputOutputLibrary/TestDLL/TestDLL.cpp
@@ -0,0 +1,12 @@
+// TestDLL.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <InputOutputLibrary.h>
+
+int main()
+{
+	mp3_to_wav(L"C:\\Users\\gabon\\_Music\\wanna_be_like_you_sim_gretina.mp3", L"C:\\Users\\gabon\\_Music\\wav\\wanna_be_like_you_sim_gretina.wav");
+    return 0;
+}
+
