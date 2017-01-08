03b306ef2bdd5400a3880b48810bfd2210f70801
Maxime Stevenot <stevenot@ecole.ensicaen.fr>
2017-01-08 16:37:50 +0100

    Add licence into AutoMix UI and AutoMix DataManagement

--- AutoMix Software/AutoMix Data Management/AudioDataExtractionMock.cpp
+++ AutoMix Software/AutoMix Data Management/AudioDataExtractionMock.cpp
@@ -1,4 +1,12 @@
-﻿#include "stdafx.h"
+﻿// Copyright (C) 2016-2017 LesProjecteurs - All Rights Reserved
+// Maxime STEVENOT, Guillaume HANNES, Jordan ERNULT, Louis CARLIER, Pierre GABON
+// 
+// This file is part of AutoMix Software.
+// 
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// You should have received a copy of the License along with this program.
+
+#include "stdafx.h"
 #include "AudioDataExtractionMock.h"
 
 using namespace System;
