4c5b8f5a326e7a8e4a033f73c62f2c23fc399d42
Jordan Ernult <ernult@ecole.ensicaen.fr>
2017-01-27 16:54:53 +0100

    unitary test project added

---
+++ AutoMix Software/AutoMix Tests/AutoMix Tests.h
@@ -0,0 +1,19 @@
+// Copyright (C) 2016-2017 LesProjecteurs - All Rights Reserved
+// Maxime STEVENOT, Guillaume HANNES, Jordan ERNULT, Louis CARLIER, Pierre GABON
+// 
+// This file is part of AutoMix Software.
+// 
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// You should have received a copy of the License along with this program.
+
+#pragma once
+
+using namespace System;
+
+namespace AutoMixTests {
+
+	public ref class Class1
+	{
+		// TODO: ajoutez ici vos méthodes pour cette classe.
+	};
+}
