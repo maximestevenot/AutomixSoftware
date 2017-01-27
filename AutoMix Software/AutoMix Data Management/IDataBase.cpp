77464c81e8db2a58ddff842cf0c7f7828b46c3cc
Jordan Ernult <ernult@ecole.ensicaen.fr>
2017-01-27 16:41:17 +0100

    headers modif

--- AutoMix Software/AutoMix Data Management/IDataBase.cpp
+++ AutoMix Software/AutoMix Data Management/IDataBase.cpp
@@ -1,3 +1,11 @@
+// Copyright (C) 2016-2017 LesProjecteurs - All Rights Reserved
+// Maxime STEVENOT, Guillaume HANNES, Jordan ERNULT, Louis CARLIER, Pierre GABON
+// 
+// This file is part of AutoMix Software.
+// 
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// You should have received a copy of the License along with this program.
+
 #include "stdafx.h"
 #include "IDataBase.h"
 
@@ -7,7 +15,7 @@ using namespace System::Collections::Generic;
 using namespace AutoMixDataManagement;
 
 namespace DataBase {
-
+	
 	IDataBase::IDataBase()
 	{
 		
