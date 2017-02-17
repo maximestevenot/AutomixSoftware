20bd66e7edceba8a2126ac21476655e49dc5f511
Maxime Stevenot <stevenot@ecole.ensicaen.fr>
2017-02-17 18:56:33 +0100

    check before launching algo

--- AutoMix Software/AutoMix UI/Presenter.cpp
+++ AutoMix Software/AutoMix UI/Presenter.cpp
@@ -35,8 +35,11 @@ namespace AutoMixUI {
 
 	TrackCollection^ Presenter::sortTrackCollectionWithGeneticAlgorithm(ComponentModel::BackgroundWorker^ bw)
 	{
-		GeneticAlgorithm^ _geneticAlgorithm = gcnew GeneticAlgorithm();
-		_trackCollection = _geneticAlgorithm->sortTrackByGeneticAlgorithm(bw, _trackCollection);
+		if (_trackCollection->Count > 1)
+		{
+			GeneticAlgorithm^ _geneticAlgorithm = gcnew GeneticAlgorithm();
+			_trackCollection = _geneticAlgorithm->sortTrackByGeneticAlgorithm(bw, _trackCollection);
+		}
 		return _trackCollection;
 	}
 
