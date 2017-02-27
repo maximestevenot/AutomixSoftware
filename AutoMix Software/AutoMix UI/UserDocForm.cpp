ddf19c3b57350f767edaad4f171e829efd7d7a19
Maxime Stevenot <stevenot@ecole.ensicaen.fr>
2017-02-27 21:27:53 +0100

    done

--- AutoMix Software/AutoMix UI/UserDocForm.cpp
+++
@@ -1,2 +0,0 @@
-#include "UserDocForm.h"
-
--- AutoMix Software/AutoMix UI/UserDocForm.h
+++ AutoMix Software/AutoMix UI/UserDocForm.h
@@ -1,3 +1,11 @@
+// Copyright (C) 2016-2017 LesProjecteurs - All Rights Reserved
+// Maxime STEVENOT, Guillaume HANNES, Jordan ERNULT, Louis CARLIER, Pierre GABON
+// 
+// This file is part of AutoMix Software.
+// 
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// You should have received a copy of the License along with this program.
+
 #pragma once
 
 namespace AutoMixUI {
@@ -18,9 +26,6 @@ namespace AutoMixUI {
 		UserDocForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
 		}
 
 	protected:
@@ -44,6 +49,8 @@ namespace AutoMixUI {
 	private: System::Windows::Forms::Button^  okButton;
 	private: System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel2;
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::RichTextBox^  _keyDescription;
+	private: System::Windows::Forms::RichTextBox^  richTextBox1;
 
 
 
@@ -64,12 +71,15 @@ namespace AutoMixUI {
 			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
 			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
 			this->_bpmTab = (gcnew System::Windows::Forms::TabPage());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
 			this->_keyPage = (gcnew System::Windows::Forms::TabPage());
 			this->tableLayoutPanel2 = (gcnew System::Windows::Forms::TableLayoutPanel());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->_keyDescription = (gcnew System::Windows::Forms::RichTextBox());
 			this->okButton = (gcnew System::Windows::Forms::Button());
 			this->tableLayoutPanel1->SuspendLayout();
 			this->tabControl1->SuspendLayout();
+			this->_bpmTab->SuspendLayout();
 			this->_keyPage->SuspendLayout();
 			this->tableLayoutPanel2->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
@@ -82,7 +92,7 @@ namespace AutoMixUI {
 			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
 				100)));
 			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
-				145)));
+				107)));
 			this->tableLayoutPanel1->Controls->Add(this->tabControl1, 0, 0);
 			this->tableLayoutPanel1->Controls->Add(this->okButton, 1, 1);
 			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
@@ -109,6 +119,7 @@ namespace AutoMixUI {
 			// 
 			// _bpmTab
 			// 
+			this->_bpmTab->Controls->Add(this->richTextBox1);
 			this->_bpmTab->Location = System::Drawing::Point(4, 22);
 			this->_bpmTab->Name = L"_bpmTab";
 			this->_bpmTab->Padding = System::Windows::Forms::Padding(3);
@@ -117,8 +128,25 @@ namespace AutoMixUI {
 			this->_bpmTab->Text = L"BPM";
 			this->_bpmTab->UseVisualStyleBackColor = true;
 			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->BackColor = System::Drawing::SystemColors::Window;
+			this->richTextBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->richTextBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->richTextBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->richTextBox1->Location = System::Drawing::Point(3, 3);
+			this->richTextBox1->Margin = System::Windows::Forms::Padding(10);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->ReadOnly = true;
+			this->richTextBox1->ShortcutsEnabled = false;
+			this->richTextBox1->Size = System::Drawing::Size(1116, 535);
+			this->richTextBox1->TabIndex = 2;
+			this->richTextBox1->Text = resources->GetString(L"richTextBox1.Text");
+			// 
 			// _keyPage
 			// 
+			this->_keyPage->AutoScroll = true;
 			this->_keyPage->Controls->Add(this->tableLayoutPanel2);
 			this->_keyPage->Location = System::Drawing::Point(4, 22);
 			this->_keyPage->Name = L"_keyPage";
@@ -127,7 +155,6 @@ namespace AutoMixUI {
 			this->_keyPage->TabIndex = 1;
 			this->_keyPage->Text = L"Key";
 			this->_keyPage->UseVisualStyleBackColor = true;
-			this->_keyPage->AutoScroll = true;
 			// 
 			// tableLayoutPanel2
 			// 
@@ -138,6 +165,7 @@ namespace AutoMixUI {
 			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
 				50)));
 			this->tableLayoutPanel2->Controls->Add(this->pictureBox1, 0, 0);
+			this->tableLayoutPanel2->Controls->Add(this->_keyDescription, 1, 0);
 			this->tableLayoutPanel2->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->tableLayoutPanel2->Location = System::Drawing::Point(3, 3);
 			this->tableLayoutPanel2->Name = L"tableLayoutPanel2";
@@ -157,11 +185,27 @@ namespace AutoMixUI {
 			this->pictureBox1->TabIndex = 0;
 			this->pictureBox1->TabStop = false;
 			// 
+			// _keyDescription
+			// 
+			this->_keyDescription->BackColor = System::Drawing::SystemColors::Window;
+			this->_keyDescription->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->_keyDescription->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->_keyDescription->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->_keyDescription->Location = System::Drawing::Point(568, 10);
+			this->_keyDescription->Margin = System::Windows::Forms::Padding(10);
+			this->_keyDescription->Name = L"_keyDescription";
+			this->_keyDescription->ReadOnly = true;
+			this->_keyDescription->ShortcutsEnabled = false;
+			this->_keyDescription->Size = System::Drawing::Size(538, 515);
+			this->_keyDescription->TabIndex = 1;
+			this->_keyDescription->Text = resources->GetString(L"_keyDescription.Text");
+			// 
 			// okButton
 			// 
 			this->okButton->Anchor = System::Windows::Forms::AnchorStyles::None;
 			this->okButton->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
-			this->okButton->Location = System::Drawing::Point(1021, 576);
+			this->okButton->Location = System::Drawing::Point(1040, 576);
 			this->okButton->Name = L"okButton";
 			this->okButton->Size = System::Drawing::Size(84, 30);
 			this->okButton->TabIndex = 1;
@@ -180,6 +224,7 @@ namespace AutoMixUI {
 			this->Text = L"AutoMix Software";
 			this->tableLayoutPanel1->ResumeLayout(false);
 			this->tabControl1->ResumeLayout(false);
+			this->_bpmTab->ResumeLayout(false);
 			this->_keyPage->ResumeLayout(false);
 			this->tableLayoutPanel2->ResumeLayout(false);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
