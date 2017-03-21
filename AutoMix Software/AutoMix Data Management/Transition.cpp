118d0a95e207d8b60e425f2f30f77b3a0998b95c
Jordan Ernult <ernult@ecole.ensicaen.fr>
2017-03-21 18:29:03 +0100

    duration fixed

--- AutoMix Software/AutoMix Data Management/Transition.cpp
+++ AutoMix Software/AutoMix Data Management/Transition.cpp
@@ -34,12 +34,15 @@ void AutoMixDataManagement::Transition::makeTransition()
 		}
 		previousTrack = track;
 	}
+
 }
 
 void AutoMixDataManagement::Transition::fadeIn(Track ^ track)
 {
 	Mp3FileReader^ fileReader = gcnew Mp3FileReader(track->Path);
-	array<float>^ bytes = gcnew array<float>((long)fileReader->Length);
+	array<float>^ bytes = gcnew array<float>((long)fileReader->Length/4);
+	
+
 	FadeInOutSampleProvider^ fade = gcnew FadeInOutSampleProvider(WaveExtensionMethods::ToSampleProvider(fileReader), false);
 	fade->BeginFadeIn(10000);
 	fade->Read(bytes, 0, (long)fileReader->Length);
@@ -52,9 +55,8 @@ void AutoMixDataManagement::Transition::fadeInOut(Track ^ track)
 
 	Mp3FileReader^ fileReader = gcnew Mp3FileReader(track->Path);
 	ISampleProvider^ sample = WaveExtensionMethods::ToSampleProvider(fileReader);
-	//sample->WaveFormat
-	int size =(int) ((long)fileReader->Length / sample->WaveFormat->BitsPerSample);
-	array<float>^ bytes = gcnew array<float>(size);
+	long size = fileReader->Length / 4;
+	array<float>^ bytes = gcnew array<float>((long)fileReader->Length / 4);
 	FadeInOutSampleProvider^ fade = gcnew FadeInOutSampleProvider(WaveExtensionMethods::ToSampleProvider(fileReader), false);
 	fade->BeginFadeIn(10000);
 	int bytems = fade->WaveFormat->AverageBytesPerSecond / 1000;
@@ -76,7 +78,7 @@ void AutoMixDataManagement::Transition::fadeOut(Track ^ track)
 	Mp3FileReader^ fileReader = gcnew Mp3FileReader(track->Path);
 
 	_waveFileWriter = gcnew WaveFileWriter(System::IO::Path::GetTempPath() + "AutomixSoftware/" + "AutoMix.wav", fileReader->WaveFormat);
-	array<float>^ bytes = gcnew array<float>((long)fileReader->Length);
+	array<float>^ bytes = gcnew array<float>((long)fileReader->Length/4);
 	FadeInOutSampleProvider^ fade = gcnew FadeInOutSampleProvider(WaveExtensionMethods::ToSampleProvider(fileReader), false);
 	int bytems = fade->WaveFormat->AverageBytesPerSecond / 1000;
 	
