103642bcc3a40efbc4bf9f3f564c0f513407b0eb
Maxime Stevenot <stevenot@ecole.ensicaen.fr>
2017-03-27 21:26:31 +0200

    Finish about form

--- AutoMix Software/AutoMix UI/AboutForm.h
+++ AutoMix Software/AutoMix UI/AboutForm.h
@@ -18,8 +18,6 @@ namespace AutoMixUI {
 		AboutForm(void)
 		{
 			InitializeComponent();
-			richTextBox1->SelectAll();
-			richTextBox1->SelectionAlignment = HorizontalAlignment::Center;
 		}
 
 	protected:
@@ -37,7 +35,7 @@ namespace AutoMixUI {
 	protected:
 	private: System::Windows::Forms::Panel^  panel2;
 	private: System::Windows::Forms::Button^  okButton;
-	private: System::Windows::Forms::PictureBox^  pictureBox2;
+
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
 	private: System::Windows::Forms::RichTextBox^  richTextBox1;
 
@@ -57,53 +55,44 @@ namespace AutoMixUI {
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(AboutForm::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
-			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->panel2 = (gcnew System::Windows::Forms::Panel());
 			this->okButton = (gcnew System::Windows::Forms::Button());
 			this->panel1->SuspendLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->panel2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// panel1
 			// 
-			resources->ApplyResources(this->panel1, L"panel1");
 			this->panel1->BackColor = System::Drawing::Color::White;
 			this->panel1->Controls->Add(this->richTextBox1);
-			this->panel1->Controls->Add(this->pictureBox2);
 			this->panel1->Controls->Add(this->pictureBox1);
 			this->panel1->Controls->Add(this->panel2);
+			resources->ApplyResources(this->panel1, L"panel1");
 			this->panel1->Name = L"panel1";
 			// 
 			// richTextBox1
 			// 
-			resources->ApplyResources(this->richTextBox1, L"richTextBox1");
 			this->richTextBox1->BackColor = System::Drawing::SystemColors::Window;
 			this->richTextBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			resources->ApplyResources(this->richTextBox1, L"richTextBox1");
 			this->richTextBox1->Name = L"richTextBox1";
 			this->richTextBox1->ReadOnly = true;
 			this->richTextBox1->ShortcutsEnabled = false;
 			// 
-			// pictureBox2
-			// 
-			resources->ApplyResources(this->pictureBox2, L"pictureBox2");
-			this->pictureBox2->Name = L"pictureBox2";
-			this->pictureBox2->TabStop = false;
-			// 
 			// pictureBox1
 			// 
-			resources->ApplyResources(this->pictureBox1, L"pictureBox1");
 			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
+			resources->ApplyResources(this->pictureBox1, L"pictureBox1");
 			this->pictureBox1->Name = L"pictureBox1";
 			this->pictureBox1->TabStop = false;
 			// 
 			// panel2
 			// 
-			resources->ApplyResources(this->panel2, L"panel2");
 			this->panel2->BackColor = System::Drawing::SystemColors::Control;
 			this->panel2->Controls->Add(this->okButton);
+			resources->ApplyResources(this->panel2, L"panel2");
 			this->panel2->Name = L"panel2";
 			// 
 			// okButton
@@ -111,6 +100,7 @@ namespace AutoMixUI {
 			resources->ApplyResources(this->okButton, L"okButton");
 			this->okButton->Name = L"okButton";
 			this->okButton->UseVisualStyleBackColor = true;
+			this->okButton->Click += gcnew System::EventHandler(this, &AboutForm::okButton_Click);
 			// 
 			// AboutForm
 			// 
@@ -122,12 +112,15 @@ namespace AutoMixUI {
 			this->MinimizeBox = false;
 			this->Name = L"AboutForm";
 			this->panel1->ResumeLayout(false);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->panel2->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
+	private: System::Void okButton_Click(System::Object^  sender, System::EventArgs^  e)
+	{
+		this->Close();
+	}
 	};
 }
