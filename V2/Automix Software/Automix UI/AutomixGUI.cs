70b8c25f776b81a8addd76ace588fb65fa985679
Maxime Stevenot <stevenot@ecole.ensicaen.fr>
2017-08-23 20:55:06 +0200

    first steps

--- V2/Automix Software/Automix UI/AutomixGUI.cs
+++ V2/Automix Software/Automix UI/AutomixGUI.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace Automix_UI
 {
-    static class Program
+    static class AutomixGUI
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +16,7 @@ namespace Automix_UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
--- V2/Automix Software/Automix UI/MainForm.Designer.cs
+++ V2/Automix Software/Automix UI/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Automix_UI
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
