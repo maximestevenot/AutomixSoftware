46cc958756f5763916d1c7faade0d7b47f0946b1
Maxime Stevenot <stevenot@ecole.ensicaen.fr>
2017-08-24 23:29:25 +0200

    Mp3Playing //TODO review this class!

---
+++ V2/Automix Software/Automix Data Management/Audio Playing/Mp3Player.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Automix_Data_Management.Audio_Playing
+{
+    //TODO review this class!
+    public class Mp3Player
+    {
+        private readonly IWavePlayer _waveOutDevice;
+        private readonly AudioFileReader _audioFileReader;
+
+        public Mp3Player(string file)
+        {
+            _waveOutDevice = new WaveOut();
+            _audioFileReader = new AudioFileReader(file);
+            _waveOutDevice.Init(_audioFileReader);
+        }
+
+        public void Play()
+        {
+            _waveOutDevice.Play();
+        }
+
+        public void Pause()
+        {
+            _waveOutDevice.Pause();
+        }
+
+        public void Stop()
+        {
+            _waveOutDevice.Stop();
+            SetPosition(0.0);
+        }
+        public long GetLength()
+        {
+            return _audioFileReader.Length;
+        }
+
+        public long GetPosition()
+        {
+            return _audioFileReader.Position;
+        }
+        public void Seek(double seconds)
+        {
+            _audioFileReader.Position = _audioFileReader.Position + (long)(seconds * _audioFileReader.WaveFormat.AverageBytesPerSecond);
+        }
+
+        public void SetPosition(long position)
+        {
+            var adj = position % _audioFileReader.WaveFormat.BlockAlign;
+            _audioFileReader.Position = Math.Max(0, Math.Min(_audioFileReader.Length, position - adj));
+        }
+        public void SetPosition(double seconds)
+        {
+            _audioFileReader.Position = (long)(seconds * _audioFileReader.WaveFormat.AverageBytesPerSecond);
+        }
+        public void SetPosition(TimeSpan timeSpan)
+        {
+            _audioFileReader.Position = (long)timeSpan.TotalSeconds;
+        }
+
+    }
+}
