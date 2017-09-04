7ec0cf482cab9107d339345d966f142513e67e8b
Maxime Stevenot <stevenot@ecole.ensicaen.fr>
2017-09-04 17:13:26 +0200

    add header

--- Automix AI Tests/Distances/SimpleTracksDistanceTest.cs
+++ Automix AI Tests/Distances/SimpleTracksDistanceTest.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// Copyright (C) 2016-2017 LesProjecteurs - All Rights Reserved
+// Maxime STEVENOT, Guillaume HANNES, Jordan ERNULT, Louis CARLIER, Pierre GABON
+// 
+// This file is part of Automix Software.
+// 
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// You should have received a copy of the License along with this program.
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Automix_AI.Distances;
 using Automix_Data_Management.Model;
