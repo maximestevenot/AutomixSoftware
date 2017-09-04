bc6fb2d0ccf07e9178e11cc3020a303e8f8f28c7
Maxime Stevenot <stevenot@ecole.ensicaen.fr>
2017-09-04 17:07:51 +0200

    tests implemented

---
+++ Automix AI Tests/Distances/SimpleTracksDistanceTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Automix_AI.Distances;
+using Automix_Data_Management.Model;
+
+namespace Automix_AI_Tests.Distances
+{
+    [TestClass]
+    public class SimpleTracksDistanceTest
+    {
+        [TestMethod]
+        public void TestCompute()
+        {
+            ITracksDistance distance = new SimpleTracksDistance();
+
+            var track1 = new Track()
+            {
+                Bpm = 128,
+                Key = "6d"
+            };
+
+            var track2 = new Track()
+            {
+                Bpm = 150,
+                Key = "6m"
+            };
+
+            var track3 = new Track()
+            {
+                Bpm = 128,
+                Key = "6m"
+            };
+
+
+            var value1 = distance.Compute(track1, track2);
+            Assert.IsTrue(value1 >= 0);
+
+            var value2 = distance.Compute(track1, track3);
+            Assert.IsTrue(value2 >= 0);
+            Assert.IsTrue(value1 >= value2);
+        }
+    }
+}
---
+++ Automix AI Tests/Sort Alogirthms/SimulatedAnnealingSortAlgorithmTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using Automix_AI.Distances;
+using Automix_AI.Sort_Algorithms;
+using Automix_Data_Management.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Automix_AI_Tests.Sort_Alogirthms
+{
+    [TestClass]
+    public class SimulatedAnnealingSortAlgorithmTest
+    {
+        private static TrackCollection _trackCollection;
+        private static BackgroundWorker _backgroundWorker;
+
+        [ClassInitialize()]
+        public static void InitializeFields(TestContext testContext)
+        {
+            var track1 = new Track()
+            {
+                Bpm = 128,
+                Key = "6d"
+            };
+
+            var track2 = new Track()
+            {
+                Bpm = 150,
+                Key = "6m"
+            };
+
+            var track3 = new Track()
+            {
+                Bpm = 128,
+                Key = "6m"
+            };
+
+            _trackCollection = new TrackCollection { track1, track2, track3 };
+
+            _backgroundWorker = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+        }
+
+        [TestMethod]
+        public void TestSortWithSimpleDistance()
+        {
+            Assert.IsNotNull(_trackCollection, "TrackCollection not initialized");
+            Assert.IsNotNull(_backgroundWorker, "BackgroundWorker not initialized");
+
+            var sortAlgorithm = new SimulatedAnnealingSortAlgorithm(new SimpleTracksDistance());
+            var resultTrackCollection = sortAlgorithm.Sort(_backgroundWorker, _trackCollection);
+
+            Assert.AreEqual(_trackCollection.Count, resultTrackCollection.Count);
+        }
+    }
+}
