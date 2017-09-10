e26cd1b84b8ca36aef8a0114970cd92125ff3288
Maxime Stevenot <stevenot@ecole.ensicaen.fr>
2017-09-10 14:10:59 +0200

    enhance product version string

--- Automix UI/Forms/AboutForm.cs
+++ Automix UI/Forms/AboutForm.cs
@@ -17,8 +17,20 @@ namespace Automix_UI.Forms
         {
             InitializeComponent();
             var text = richTextBox1.Text;
-            richTextBox1.Text = string.Format(text,Application.ProductVersion);
+            richTextBox1.Text = string.Format(text, GetEnhancedProductVersion());
         }
+
+        private static string GetEnhancedProductVersion()
+        {
+            var productVersion = Application.ProductVersion;
+            var lastDotIndex = productVersion.LastIndexOf(".", StringComparison.Ordinal);
+            if (lastDotIndex > 0)
+            {
+                productVersion = productVersion.Substring(0, lastDotIndex);
+            }
+            return productVersion;
+        }
+
         private void OnOkButtonClick(object sender, EventArgs e) => Close();
     }
 }
